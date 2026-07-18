83adb9191028bf9e8d1288ef13f03267f97c1670
Ali Abbasiyan <Ali.Abbasiyan94@gmail.com>
2026-07-19 02:04:08 +0330

    Add initial API ingestion and project dependencies

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Data Engineering Product Pipeline")
+import requests
+
+url = "https://dummyjson.com/products"
+
+response = requests.get(url, timeout=30)
+
+data = response.json()
+
+print("Status Code:", response.status_code)
+print("Total Products:", data["total"])
+print("Returned Products:", len(data["products"]))
+print("Skip:", data["skip"])
+print("Limit:", data["limit"])
